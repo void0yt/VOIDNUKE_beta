c91312ccf0a79a3aac1da8611d1f4593a7e27077
void0yt <93957789+void0yt@users.noreply.github.com>
2021-11-09 09:39:46 +0800

    Add files via upload

---
+++ VOIDNUKE.py
@@ -0,0 +1,60 @@
+import subprocess
+import time
+
+print("===========================")
+print("        VoidNuke           ")
+print("===========================")
+print("Created by void(0); on YouTube")
+print("Loading...")
+
+time.sleep(5)
+
+print("Successfully Loaded!")
+
+time.sleep(1)
+
+var1 = input("Press a key to open a window... ")
+
+print(var1)
+subprocess.Popen('C:\\Windows\\System32\\write.exe')
+time.sleep(3)
+print("!!!PROCEED AT YOUR OWN RISK!!!")
+var2 = input("Enable nuke? Y/N ")
+if var2 == ("N"):
+    print("TERMINATING...")
+    time.sleep(1)
+    var3 = input("Press any key to close this window ")
+elif var2 == ("Y"):
+    print("ENABLING NUKE...")
+    time.sleep(2)
+    while 5 > 2:
+
+        subprocess.Popen('C:\\Windows\\System32\\write.exe')
+
+#This is only for educational purposes. I am not responsible for any damage to your computer.
+#Use at your own risk.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
